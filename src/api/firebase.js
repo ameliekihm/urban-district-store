aadbeab3bc6d809de6a165861339c84bab963fdf
ameliekihm <ameliekihm@gmail.com>
2025-07-15 01:13:05 -0600

    Fix: corrected Firebase env variable name to DATABASE_URL

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,4 +1,3 @@
-console.log('✅ DB URL:', process.env.REACT_APP_FIREBASE_DB_URL);
 import { initializeApp } from 'firebase/app';
 import { v4 as uuid } from 'uuid';
 import {
@@ -14,14 +13,14 @@ import { getDatabase, ref, get, set, remove } from 'firebase/database';
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
-
+console.log('✅ DB URL:', firebaseConfig.databaseURL);
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
